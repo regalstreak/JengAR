028d21955eccb0c8a09f6965d99f622a12de8f7b
Neil Agarwal <neil@neilagarwal.me>
2020-04-26 02:49:28 +0530

    JengAR: Initial commit with some basic ARFoundation plane detection

---
+++ Assets/Scripts/PlacementIndicator.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.ARFoundation;
+using UnityEngine.XR.ARSubsystems;
+
+public class PlacementIndicator : MonoBehaviour {
+
+    private GameObject visual;
+    private ARRaycastManager raycastManager;
+
+    void Start() {
+        raycastManager = FindObjectOfType<ARRaycastManager>();
+        visual = transform.GetChild(0).gameObject;
+        visual.SetActive(false);
+    }
+
+    void Update() {
+        List<ARRaycastHit> hits = new List<ARRaycastHit>();
+        raycastManager.Raycast(new Vector2(Screen.width / 2, Screen.height / 2), hits, TrackableType.Planes);
+
+        if (hits.Count > 0) {
+            transform.position = hits[0].pose.position;
+            transform.rotation = hits[0].pose.rotation;
+            if (!visual.activeInHierarchy) {
+                visual.SetActive(true);
+            }
+        }
+    }
+}
