bdb7f96ef1ac2ff883de2f19d7c72d0a521979e8
Neil Agarwal <neil@neilagarwal.me>
2020-04-26 04:27:09 +0530

    JengAR: Object placement in AR

---
+++ Assets/Scripts/JengaPlacement.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JengaPlacement : MonoBehaviour {
+    public GameObject jengaToSpawn;
+
+    private PlacementIndicator placementIndicator;
+
+    void Start() {
+        placementIndicator = FindObjectOfType<PlacementIndicator>();
+    }
+
+    void Update() {
+        if (Input.touchCount > 0 && Input.touches[0].phase == TouchPhase.Began) {
+            GameObject obj = Instantiate(jengaToSpawn, placementIndicator.transform.position, placementIndicator.transform.rotation);
+        }
+    }
+}
